b003eac4111ac434ce5357d1d5cec48ec5504a86
nnalin04 <nnalin04@gmail.com>
2020-08-02 20:25:52 +0530

    added: UC4 - finction to display the board

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,35 +2,38 @@ import java.util.*;
 
 class TicTacToe {
 
-    public  static void main(final String[] args) {
+    public static void main(final String[] args) {
 
-        //creating an object of PlayingTTT class.
+        // creating an object of PlayingTTT class.
         final PlayingTTT play = new PlayingTTT();
         Scanner sc = new Scanner(System.in);
 
-        //creating the board as an 3*3 array
+        // creating the board as an 3*3 array
         final String[][] board = new String[3][3];
 
-        //calling the function to set the re-set the board.
+        // calling the function to set the re-set the board.
         play.settingBoard(board);
 
         String player;
         String cpu;
 
-        //calling a function to decide who will play first amd there symbol choice.
-        if(play.decidingToss()){
+        // calling a function to display the board.
+        play.displayBoard(board);
+
+        // calling a function to decide who will play first amd there symbol choice.
+        if (play.decidingToss()) {
             System.out.println("Enter a character between X or O");
             player = sc.next();
-            if(player == "X"){
+            if (player == "X") {
                 cpu = "O";
-            }else{
+            } else {
                 cpu = "X";
             }
-        }else{
-            if(play.decidingToss()){
+        } else {
+            if (play.decidingToss()) {
                 cpu = "O";
                 player = "X";
-            }else{
+            } else {
                 cpu = "X";
                 player = "O";
             }
@@ -41,18 +44,28 @@ class TicTacToe {
 
 class PlayingTTT {
 
-    //function to re-set the board.
+    // function to re-set the board.
     public void settingBoard(final String[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                board[i][j] = ". ";
+                board[i][j] = " .";
             }
         }
     }
 
-    //function to create a random toss
-    public boolean decidingToss(){
+    // function to create a random toss
+    public boolean decidingToss() {
         final Random r = new Random();
         return r.nextBoolean();
+    }
+
+    // function to display the board.
+    public void displayBoard(String[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
